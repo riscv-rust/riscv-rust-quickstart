f907db7636d24c462e266768cf13fd51bc97b73b
Vadim Kaushan <admin@disasm.info>
2019-03-14 18:32:34 +0300

    Add 'hello_world' example

---
+++ examples/hello_world.rs
@@ -0,0 +1,35 @@
+#![no_std]
+#![no_main]
+
+extern crate panic_halt;
+
+use riscv_rt::entry;
+use hifive1::hal::prelude::*;
+use hifive1::hal::e310x;
+use hifive1::hal::stdout::*;
+
+#[entry]
+fn main() -> ! {
+    let p = e310x::Peripherals::take().unwrap();
+
+    // Configure clocks
+    let clocks = hifive1::clock::configure(p.PRCI, p.AONCLK, 320.mhz().into());
+
+    // Configure UART
+    let mut gpio = p.GPIO0.split();
+    let (tx, rx) = hifive1::tx_rx(
+        gpio.pin17,
+        gpio.pin16,
+        &mut gpio.out_xor,
+        &mut gpio.iof_sel,
+        &mut gpio.iof_en
+    );
+    let serial = Serial::uart0(p.UART0, (tx, rx), 115_200.bps(), clocks);
+    let (mut tx, _) = serial.split();
+
+    let mut stdout = Stdout(&mut tx);
+
+    writeln!(stdout, "hello world!").unwrap();
+
+    loop {}
+}
