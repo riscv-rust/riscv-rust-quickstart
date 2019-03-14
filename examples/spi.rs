f5be93ac8abff8dc132e8adec65015ae4ecf74bc
Vadim Kaushan <admin@disasm.info>
2019-03-14 18:50:44 +0300

    Add 'spi' example

---
+++ examples/spi.rs
@@ -0,0 +1,37 @@
+#![no_std]
+#![no_main]
+
+extern crate panic_halt;
+
+use riscv_rt::entry;
+use hifive1::hal::prelude::*;
+use hifive1::hal::e310x::Peripherals;
+use hifive1::hal::spi::{Spi, Mode, Polarity, Phase};
+
+#[entry]
+fn main() -> ! {
+    let p = Peripherals::take().unwrap();
+
+    // Configure clocks
+    let clocks = hifive1::clock::configure(p.PRCI, p.AONCLK, 320.mhz().into());
+
+    // Configure SPI pins
+    let mut gpio = p.GPIO0.split();
+    let mosi = gpio.pin3.into_iof0(&mut gpio.out_xor, &mut gpio.iof_sel, &mut gpio.iof_en);
+    let miso = gpio.pin4.into_iof0(&mut gpio.out_xor, &mut gpio.iof_sel, &mut gpio.iof_en);
+    let sck = gpio.pin5.into_iof0(&mut gpio.out_xor, &mut gpio.iof_sel, &mut gpio.iof_en);
+    let cs = gpio.pin2.into_iof0(&mut gpio.out_xor, &mut gpio.iof_sel, &mut gpio.iof_en);
+
+    // Configure SPI
+    let pins = (mosi, miso, sck, cs);
+    let mode0 = Mode {
+        polarity: Polarity::IdleLow,
+        phase: Phase::CaptureOnFirstTransition,
+    };
+    let mut spi = Spi::spi1(p.QSPI1, pins, mode0, 1_000_000.hz(), clocks);
+
+    let mut buf = [0x41, 0x42, 0xab, 0xcd];
+    let _ = spi.transfer(&mut buf);
+
+    loop {}
+}
