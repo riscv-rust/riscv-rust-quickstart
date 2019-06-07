d9e8dc27f7e8b5614660cfae001fd35a3b4a103f
rmsthebest <11652273+rmsthebest@users.noreply.github.com>
2019-06-08 00:43:32 +0200

    Use HAL for interrupt instead of using unsafe blocks

--- examples/leds_blink.rs
+++ examples/leds_blink.rs
@@ -8,13 +8,12 @@
 
 extern crate panic_halt;
 
-use riscv_rt::entry;
+use hifive1::hal::e310x::Peripherals;
 use hifive1::hal::prelude::*;
 use hifive1::hal::serial::Serial;
-use hifive1::hal::e310x::Peripherals;
 use hifive1::hal::stdout::*;
 use hifive1::Led;
-use riscv::register::{mie, mip};
+use riscv_rt::entry;
 
 // switches led according to supplied status returning the new state back
 fn toggle_led(led: &mut Led, status: bool) -> bool {
@@ -40,7 +39,7 @@ fn main() -> ! {
         gpio.pin16,
         &mut gpio.out_xor,
         &mut gpio.iof_sel,
-        &mut gpio.iof_en
+        &mut gpio.iof_en,
     );
     let serial = Serial::uart0(p.UART0, (tx, rx), 115_200.bps(), clocks);
     let (mut tx, _) = serial.split();
@@ -48,9 +47,15 @@ fn main() -> ! {
     let mut stdout = Stdout(&mut tx);
 
     // get all 3 led pins in a tuple (each pin is it's own type here)
-    let mut tleds = hifive1::rgb(gpio.pin22, gpio.pin19, gpio.pin21,
-                                 &mut gpio.output_en, &mut gpio.drive,
-                                 &mut gpio.out_xor, &mut gpio.iof_en);
+    let mut tleds = hifive1::rgb(
+        gpio.pin22,
+        gpio.pin19,
+        gpio.pin21,
+        &mut gpio.output_en,
+        &mut gpio.drive,
+        &mut gpio.out_xor,
+        &mut gpio.iof_en,
+    );
 
     // get leds as the Led trait in an array so we can index them
     let ileds: [&mut Led; 3] = [&mut tleds.0, &mut tleds.1, &mut tleds.2];
@@ -62,9 +67,7 @@ fn main() -> ! {
     let mut current_led = 0; // start on red
 
     // enable timer
-    unsafe {
-        mie::set_mtimer();
-    }
+    clint.mtimer.enable();
 
     writeln!(stdout, "Starting blink loop").unwrap();
 
@@ -80,18 +83,16 @@ fn main() -> ! {
         // set next wakeup time each iteration
         clint.mtimecmp.set_mtimecmp(clint.mtime.mtime() + PERIOD);
 
-        unsafe {
+        loop {
             // Wait For Interrupt will put CPU to sleep until an interrupt hits
             // in our case when internal timer mtime value >= mtimecmp value
             // after which empty handler gets called and we go into the
             // next iteration of this loop
-            loop {
-                riscv::asm::wfi();
+            //            riscv::asm::wfi(); // unsafe
 
-                // check if we got the right interrupt cause, otherwise just loop back to wfi
-                if mip::read().mtimer() {
-                    break;
-                }
+            // check if there has been an mtimer interrupt
+            if clint.mtimer.is_pending() {
+                break;
             }
         }
     }
