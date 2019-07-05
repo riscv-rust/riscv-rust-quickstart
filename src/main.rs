731bd56c8c4c88ef4ec40cdf078978eb142d2817
Vadim Kaushan <admin@disasm.info>
2019-07-05 19:27:02 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,12 @@ extern crate panic_halt;
 
 use riscv_rt::entry;
 use hifive1::hal::prelude::*;
-use hifive1::hal::e310x::Peripherals;
+use hifive1::BoardResources;
 
 #[entry]
 fn main() -> ! {
-    let p = Peripherals::take().unwrap();
+    let board = BoardResources::take().unwrap();
+    let p = board.peripherals;
 
     // Configure clocks
     let _clocks = hifive1::clock::configure(p.PRCI, p.AONCLK, 320.mhz().into());
